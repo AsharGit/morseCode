2f2e10c537255392bdfb9b71e6bf7f51e0eb3cd2
AsharGit <32973667+AsharGit@users.noreply.github.com>
2019-05-07 19:57:17 -0500

    Add files via upload

--- Node.h
+++ Node.h
@@ -31,62 +31,17 @@ public:
 	{};
 };
 
-
-// Encode using the text file
-void encode(string word, ifstream& fileName)
-{
-	char temp;
-	string current;
-	ofstream text;
-	text.open("code.txt");
-
-	// Open text file.
-	fileName.open("morse.txt");
-
-	// Check to see if file opened successfully.
-	if (fileName.fail())
-	{
-		cerr << "Error opening file." << endl;
-		exit(1);
-	}
-
-	for (int i = 0; i < word.size(); i++)
-	{
-		while (!fileName.eof())
-		{
-			fileName >> temp >> current;
-			if (word.at(i) == temp)
-			{
-				cout << current << " ";
-				text << current << " ";
-				fileName.seekg(0, fileName.beg);
-				break;
-			}
-		}
-	}
-
-	fileName.close();
-	text.close();
-}
-
+// Read text file and create a Binary Tree with letters and codes.
 void buildTree(Node& root, ifstream& fileName)
 {
 	char letter;
 	string path;
 	Node* position = NULL;
 
-	// Open text file.
-	fileName.open("morse.txt");
-
-	// Check to see if file opened successfully.
-	if (fileName.fail())
-	{
-		cerr << "Error opening file." << endl;
-		exit(1);
-	}
 	while (!fileName.eof())
 	{
 		fileName >> letter >> path;
+		// Initial starting position.
 		if (path.at(0) == '.')
 		{
 			if (root.leftSubTree == NULL)
@@ -103,6 +58,8 @@ void buildTree(Node& root, ifstream& fileName)
 			}
 			position = root.rightSubTree;
 		}
+
+		// Follow the rest of the path.
 		for (int i = 1; i < path.size(); i++)
 		{
 			if (path.at(i) == '.')
@@ -122,30 +79,120 @@ void buildTree(Node& root, ifstream& fileName)
 				position = position->rightSubTree;
 			}
 		}
+		// Fill the data
 		position->data = letter;
 		position->code = path;
 		position = NULL;
 	}
 }
 
-void find(Node &root, char key)
+// Find the code given a letter.
+bool find(Node* rootPtr, char key)
 {
-	Node newRoot = NULL;
+	Node* position = NULL;
+	bool found = false;
 
-	if (&root == NULL)
+	// Check if tree is empty.
+	if (rootPtr == NULL)
 	{
-		cout << "Leaf Node" << endl;
+		cout << "Empty tree." << endl;
+		return false;
 	}
-	else if (root.data == key)
+	// Print and return true if key is found.
+	if (rootPtr->data == key)
 	{
-		cout << root.code << " ";
+		cout << rootPtr->code << " ";
+		return true;
 	}
-
-	else if (root.data != key)
+	else if (rootPtr->data != key)
 	{
-		newRoot = *root.leftSubTree;
-		find(newRoot, key);
+		// Look at left tree first.
+		if (rootPtr->leftSubTree != NULL)
+		{
+			position = rootPtr->leftSubTree;
+			found = find(position, key);
+		}
+		// Look at right tree if not found.
+		if (rootPtr->rightSubTree != NULL && found != true)
+		{
+			position = rootPtr->rightSubTree;
+			found = find(position, key);
+		}
 	}
+	return found;
 }
 
+// Find the letter given code.
+bool find(Node* rootPtr, string code)
+{
+	Node* position = NULL;
+	bool found = false;
 
+	// Check if tree is empty.
+	if (rootPtr == NULL)
+	{
+		cout << "Empty tree." << endl;
+		return false;
+	}
+	// Print and return true if code is found.
+	if (rootPtr->code == code)
+	{
+		cout << rootPtr->data;
+		return true;
+	}
+	else if (rootPtr->code != code)
+	{
+		// Look at left tree first.
+		if (rootPtr->leftSubTree != NULL)
+		{
+			position = rootPtr->leftSubTree;
+			found = find(position, code);
+		}
+		// Look at right tree if not found.
+		if (rootPtr->rightSubTree != NULL && found != true)
+		{
+			position = rootPtr->rightSubTree;
+			found = find(position, code);
+		}
+	}
+	return found;
+}
+
+// Create a morse code message given a word.
+void encode(Node* rootPtr, string word)
+{
+	char key;
+
+	cout << word << " in morse code: ";
+	for (int i = 0; i < word.size(); i++)
+	{
+		key = word.at(i);
+		find(rootPtr, key);
+	}
+
+	cout << endl;
+}
+
+// Create the translation of a morse code message.
+void decode(Node* rootPtr, string message)
+{
+	string code;
+
+	cout << message << " decoded: ";
+	for (int i = 0; i < message.size(); i++)
+	{
+		if (message.at(i) == ' ')
+		{
+			find(rootPtr, code);
+			code.clear();
+		}
+		else
+		{
+			code += message.at(i);
+		}
+
+	}
+	find(rootPtr, code);
+	code.clear();
+	cout << endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,16 +3,26 @@
 int main()
 {
 	Node root;
-	Node* position = NULL;
-			
+	Node* rootPtr = &root;
 	ifstream inFile;
+
+	// Open text file.
+	inFile.open("morse.txt");
+
+	// Check to see if file opened successfully.
+	if (inFile.fail())
+	{
+		cerr << "Error opening file." << endl;
+		exit(1);
+	}
+
 	buildTree(root, inFile);
-	find(root, 'h');
+	encode(rootPtr,"puppy");
+	decode(rootPtr, ".__. .._ .__. .__. _.__");
+
+	// Close text file.
+	inFile.close();
 		
-
-	/*encode("cat", inFile);*/
-
-
 	return 0;
 }
 
